885359e6a94a0af55b6ac6865bda4808c7d74a60
Alnux <alexmiravite@gmail.com>
2022-11-15 00:12:18 +0800

    day49

--- nodejs-intro/app.js
+++ nodejs-intro/app.js
@@ -1,18 +1,41 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
 
+const app = express();
+
+
+//
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>' +  new Date().toISOString() + '</h1>');
+});
+
+app.get('/', function(req, res) {
+    //use name in the input so you can extract it 
+    res.send('<form action="/store-user" method="POST"><label>Your Name<label><input type="text" name="username"><button>Submit</button></form>')
+});
+
+app.post('/store-user', function(req, res) {
+    const userName = req.body.username;
+    console.log(userName);
+    res.send('<h1>Username stored!</h1>')
+})
+
+app.listen(3000);
+
+//original node code below (not express)
 //can be named anything we want but we are naming it handleRequest
-function handleRequest(request, response) {
+// function handleRequest(request, response) {
 
-    if(request.url === '/currenttime') {
-        response.statusCode = 200;
-        response.end('<h1>' +  new Date().toISOString() + '</h1>');
-    } else if (request.url === '/') {
-        response.statusCode = 200;
-        response.end('<h1>Hello World!</h1>');
-    }
-}
+//     if(request.url === '/currenttime') {
+//         response.statusCode = 200;
+//         response.end('<h1>' +  new Date().toISOString() + '</h1>');
+//     } else if (request.url === '/') {
+//         response.statusCode = 200;
+//         response.end('<h1>Hello World!</h1>');
+//     }
+// }
 
-//createServer expects a function
-const server = http.createServer(handleRequest);
+// //createServer expects a function
+// const server = http.createServer(handleRequest);
 
-server.listen(3000);
+// server.listen(3000);
