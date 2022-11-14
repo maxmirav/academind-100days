807fdea52fb130ce36a4159481380fab680aacc3
Alexis <miravite.alexis@gmail.com>
2022-11-14 19:56:10 +0800

    intro to node day 47

--- nodejs-intro/app.js
+++ nodejs-intro/app.js
@@ -1,4 +1,18 @@
-let userName = 'Maximillian';
+const http = require('http');
 
-console.log(userName);
+//can be named anything we want but we are naming it handleRequest
+function handleRequest(request, response) {
 
+    if(request.url === '/currenttime') {
+        response.statusCode = 200;
+        response.end('<h1>' +  new Date().toISOString() + '</h1>');
+    } else if (request.url === '/') {
+        response.statusCode = 200;
+        response.end('<h1>Hello World!</h1>');
+    }
+}
+
+//createServer expects a function
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
